2aa0ed9112920ea5cb6bd0242e81f6f5fa616aa8
Ygilvan <gilvansc1234@gmail.com>
2023-04-27 15:49:04 -0300

    second commit

--- src/notas/Notas.java
+++ src/notas/Notas.java
@@ -15,28 +15,29 @@ public class Notas {
                 double grade=input.nextDouble();
                 addGrade(grade);
             } catch (Exception e) {
-                System.out.println("");
+                System.out.println("input a valid grade");
                 input.next();
             }
             System.out.print("one more grade? (Y/N)");
         }while(input.next().contains("y"));
-        System.out.println("grades quantity: "+grades.size());
-        System.out.println("grades: "+grades);
-        System.out.println("grades media: "+total/grades.size());
-        if (total/grades.size()>=6.0)
-            System.out.println("approved!");
-        else System.out.println("reproved!");
-
+    result();
     }
 
     private static void addGrade(double grade){
-        if (isValidGrade(grade)){
+        if (isValidGrade(grade)) {
             grades.add(grade);
-            total+=grade;
+            total += grade;
         }
     }
     private static boolean isValidGrade (double grade) {
         return grade<=10 && grade>=0;
     }
-
+    private static void result(){
+//        System.out.println("grades quantity: "+grades.size());
+//        System.out.println("grades: "+grades);
+        System.out.printf("grades media: %.2f || Result: ",total/grades.size());
+        if (total/grades.size()>=6.0)
+            System.out.println("approved!!!");
+        else System.out.println("reproved!!!");
+    }
 }
